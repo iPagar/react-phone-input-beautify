89d13a2b0525efdc93809864237bb569c937ddb9
Pavel Garaev <ipagar@icloud.com>
2024-02-06 12:24:18 +0400

    feat: add semantic release

---
+++ release.config.cjs
@@ -0,0 +1,23 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+};
