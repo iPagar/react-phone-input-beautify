180df7d5cc0f4fcb09688e8f1b32b7092d9e4ac2
GitHub Action <action@github.com>
2024-02-21 09:50:46 +0000

    chore: add build assets [skip ci]

--- lib/use-phone-state/use-phone-state.d.ts
+++ lib/use-phone-state/use-phone-state.d.ts
@@ -1,8 +1,10 @@
+import { CountryCode } from 'libphonenumber-js';
 import z from 'zod';
 export declare const phoneValidationSchema: ({ invalid, required }?: {
     invalid: string;
     required: string;
 }) => z.ZodEffects<z.ZodString, string, string>;
+export declare function getCountryCode(phone: string): "" | CountryCode | undefined;
 export declare function formatPhoneNumber(phoneNumber: string, country?: string): string | undefined;
 /**
  * @param initialCountry - format is ISO 3166-1 alpha-2
